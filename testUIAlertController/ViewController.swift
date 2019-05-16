a501df46da89e3902dce8c76c47ebb7dacde0e35
Ardi Muhammad <ardi@arubygames.com>
2019-05-16 07:57:44 +0700

    Initial Commit

---
+++ testUIAlertController/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  testUIAlertController
+//
+//  Created by Ardi Muhammad Husen on 16/05/19.
+//  Copyright © 2019 Ardi Muhammad Husen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
