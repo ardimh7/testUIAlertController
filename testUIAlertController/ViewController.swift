4b6f33754dddf1d5223cf83210bc843ff091b7a4
Ardi Muhammad <ardi@arubygames.com>
2019-05-16 09:05:58 +0700

    UIAlertController complete! with two alert types.

--- testUIAlertController/ViewController.swift
+++ testUIAlertController/ViewController.swift
@@ -10,19 +10,62 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var alertBtn: UIButton!
+    @IBOutlet weak var actionSheetBtn: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-    }
-    
-    override func viewDidAppear(_ animated: Bool) {
-        let alert = UIAlertController(title: "My Alert", message: "This is an alert.", preferredStyle: .alert)
+        
+        let alert = UIAlertController(title: "Hello Dude!", message: "NC1 - Group 7 Morning Session", preferredStyle: .alert)
         alert.addAction(UIAlertAction(title: NSLocalizedString("OK", comment: "Default action"), style: .default, handler: { _ in
             NSLog("The \"OK\" alert occured.")
         }))
         self.present(alert, animated: true, completion: nil)
     }
+    
+    // GRUP 7 NC - 1
+    // UIAlertController
+    
+    
+    // deklarasi di global,
+    var alertTwo: UIAlertController!
+    var alertThree: UIAlertController!
+    
+    override func viewDidAppear(_ animated: Bool) {
+//        let alert = UIAlertController(title: "Hello Dude!", message: "NC1 - Group 7 Morning Session", preferredStyle: .alert)
+//        alert.addAction(UIAlertAction(title: NSLocalizedString("OK", comment: "Default action"), style: .default, handler: { _ in
+//            NSLog("The \"OK\" alert occured.")
+//        }))
+//        self.present(alert, animated: true, completion: nil)
+        
+        
+        // inisialisasi di lokal
+        alertTwo = UIAlertController(title: "testAnotherAlert", message: "Tipe alert .alert", preferredStyle: .alert)
+        alertTwo.addAction(UIAlertAction(title: NSLocalizedString("Oke", comment: "Default action"), style: .default, handler: { _ in
+            NSLog("The \"Oke\" alert occured.")
+        }))
+        
+        
+        alertThree = UIAlertController(title: "testAnotherAlert", message: "Tipe alert .alert", preferredStyle: .actionSheet)
+        alertThree.addAction(UIAlertAction(title: NSLocalizedString("Oke", comment: "Default action"), style: .default, handler: { _ in
+            NSLog("The \"Oke\" alert occured.")
+        }))
+        
+    }
 
+    
+    
+    
 
+    @IBAction func alertBtnAction(_ sender: Any) {
+        self.present(alertTwo, animated: true, completion: nil)
+    }
+    
+    
+    @IBAction func actionSheetBtnAction(_ sender: Any) {
+        self.present(alertThree, animated: true, completion: nil)
+    }
+    
 }
 
