78fa0dc0c13ca18d815928e6cc72b0a06432bd6f
Ardi Muhammad <ardi@arubygames.com>
2019-05-16 08:01:41 +0700

    nambah kode untuk test UIAlertController

--- testUIAlertController/ViewController.swift
+++ testUIAlertController/ViewController.swift
@@ -14,6 +14,14 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        let alert = UIAlertController(title: "My Alert", message: "This is an alert.", preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: NSLocalizedString("OK", comment: "Default action"), style: .default, handler: { _ in
+            NSLog("The \"OK\" alert occured.")
+        }))
+        self.present(alert, animated: true, completion: nil)
+    }
 
 
 }
